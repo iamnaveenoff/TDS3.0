ab1ed98009449706c573ce66eb80c9d5725456c1
iamnaveenoff <naveencreater106@gmail.com>
2022-05-14 13:44:30 +0530

    more powerful

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import bomber
+import subprocess
+
 # colored text and background
 def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
 def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
@@ -67,3 +72,34 @@ def bann_text():
 
 
 bann_text()
+
+
+menu_options = {
+    1: 'TBomb',
+    4: 'Exit',
+}
+
+def print_menu():
+    for key in menu_options.keys():
+        print (key, '--', menu_options[key] )
+
+def tbomb():
+    print("naveen")
+
+
+if __name__=='__main__':
+    while(True):
+        print_menu()
+        option = ''
+        try:
+            option = int(input('Enter your choice: '))
+        except:
+            print('Wrong input. Please enter a number ...')
+        #Check what choice was entered and act accordingly
+        if option == 1:
+            tbomb()
+        elif option == 4:
+            print('Thanks message before exiting')
+            exit()
+        else:
+            print('Invalid option. Please enter a number between 1 and 4.')
