b3ced4c2fcd7bda1e0d6ee9de870ddeb83d9efba
iamnaveenoff <naveencreater106@gmail.com>
2022-05-15 08:26:28 +0530

    code corrected

--- Start.sh
+++ Start.sh
@@ -115,7 +115,7 @@ do
     read ch
     clear
     if [ $ch -eq 1 ];then
-        python bomber.py --sms
+        echo"naveen the mass"
         exit
     elif [ $ch -eq 2 ];then
         python bomber.py --call
